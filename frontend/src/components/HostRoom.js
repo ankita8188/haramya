d05ae01e1152e2e48f92e3ef1c752e7b02abca7b
Vishesh1164 <srivastavavishesh59@gmail.com>
2025-04-19 11:34:36 +0530

    commit

--- frontend/src/components/HostRoom.js
+++ frontend/src/components/HostRoom.js
@@ -89,7 +89,7 @@ const HostRoom = () => {
   useEffect(() => {
     if (!id || !meetingRef.current) return;
 console.log( process.env.NEXT_PUBLIC_API_ID)
-console.log(process.env.SECRET_KEY)
+console.log(process.env.NEXT_PUBLIC_SECRET_KEY)
     const appId = process.env.NEXT_PUBLIC_API_ID;
     const serverSecret = process.env.NEXT_PUBLIC_SECRET_KEY;
     const userID = randomID();
