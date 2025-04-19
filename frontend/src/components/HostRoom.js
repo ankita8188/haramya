44ef591f15f278569c183136d52236debee52002
Vishesh1164 <srivastavavishesh59@gmail.com>
2025-04-19 19:25:14 +0530

    f

--- frontend/src/components/HostRoom.js
+++ frontend/src/components/HostRoom.js
@@ -101,7 +101,7 @@ console.log(process.env.NEXT_PUBLIC_SECRET_KEY)
     );
 
     const zp = ZegoUIKitPrebuilt.create(kitToken);
-    console.log(ZegoUIKitPrebuilt)
+    console.log(ZegoUIKitPrebuilt.VideoMirrorMode)
     zp.setVideoMirrorMode(ZegoUIKitPrebuilt.VideoMirrorMode.OnlyFrontCamera);
 
     zp.joinRoom({
