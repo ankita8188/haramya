0afdb32e54381db3383fd3e2aeb0f917a8d29502
Vishesh1164 <srivastavavishesh59@gmail.com>
2025-04-19 11:53:40 +0530

    ht

--- frontend/src/components/HostRoom.js
+++ frontend/src/components/HostRoom.js
@@ -26,7 +26,7 @@ const HostRoom = () => {
     }
   };
   const connectWebSocket = (isLive= false) => {
-    const websocket = new WebSocket(process.env.NEXT_PUBLIC_API_URL);
+    const websocket = new WebSocket(`ws://${NEXT_PUBLIC_API_URL}/ws`);
     websocket.onopen = () => {
       console.log('WebSocket connected');
       console.log(websocket)
